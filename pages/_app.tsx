e6168c9d69ee557136359c52be51ee2e3a75c714
Sounak Guha <65349891+DarkKingStar@users.noreply.github.com>
2023-05-18 10:18:04 +0530

    initials

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,7 +36,7 @@ useEffect(()=>{
 return (
     <AuthContextProvider>
       <Head>
-        <title>Miniacs</title>
+        <title>WRKR Miniacs</title>
         <link rel="icon" href="/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
